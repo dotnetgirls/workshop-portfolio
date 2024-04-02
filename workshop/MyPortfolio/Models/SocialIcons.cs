bb7881f5bcf4ac7ec4bf9d7022edd86286bb2227
doowon13 <83566946+doowon13@users.noreply.github.com>
2024-04-02 09:48:18 +0000

    Add my portfolio app to publish to Github Pages

---
+++ workshop/MyPortfolio/Models/SocialIcons.cs
@@ -0,0 +1,8 @@
+namespace MyPortfolio.Models;
+
+public class SocialIcons
+{
+    public string Email { get; set; } = string.Empty;
+    public string LinkedIn { get; set; } = string.Empty;
+    public string GitHub { get; set; } = string.Empty;
+}
